ee96809eab76a94964a45793bba3fbcd9b93a673
TaikiFnit <taikifnitspg@gmail.com>
2019-12-13 14:09:33 +0900

    update db scheme

--- server/database/fnit_learning_20191213.sql
+++ server/database/fnit_learning_20191213.sql
@@ -4,7 +4,7 @@
 -- https://tableplus.com/
 --
 -- Database: fnit_learning
--- Generation Time: 2019-12-11 15:27:50.7220
+-- Generation Time: 2019-12-13 14:08:49.3880
 -- -------------------------------------------------------------
 
 
@@ -18,6 +18,7 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
+DROP TABLE IF EXISTS `learning_records`;
 CREATE TABLE `learning_records` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `lesson_video_id` bigint(20) unsigned DEFAULT NULL,
@@ -28,8 +29,9 @@ CREATE TABLE `learning_records` (
   UNIQUE KEY `id` (`id`),
   KEY `lesson_video_id` (`lesson_video_id`),
   CONSTRAINT `learning_records_ibfk_1` FOREIGN KEY (`lesson_video_id`) REFERENCES `lesson_videos` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+DROP TABLE IF EXISTS `lesson_videos`;
 CREATE TABLE `lesson_videos` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `youtube_id` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
@@ -44,6 +46,7 @@ CREATE TABLE `lesson_videos` (
   CONSTRAINT `lesson_videos_ibfk_1` FOREIGN KEY (`lesson_id`) REFERENCES `lessons` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+DROP TABLE IF EXISTS `lessons`;
 CREATE TABLE `lessons` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
@@ -57,6 +60,7 @@ CREATE TABLE `lessons` (
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+DROP TABLE IF EXISTS `page_transition_logs`;
 CREATE TABLE `page_transition_logs` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `path` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
@@ -67,8 +71,9 @@ CREATE TABLE `page_transition_logs` (
   `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=349 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=619 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
@@ -78,6 +83,7 @@ CREATE TABLE `users` (
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+DROP TABLE IF EXISTS `video_feedbacks`;
 CREATE TABLE `video_feedbacks` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `feedback` text COLLATE utf8mb4_general_ci,
@@ -89,6 +95,7 @@ CREATE TABLE `video_feedbacks` (
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+DROP TABLE IF EXISTS `video_player_interaction_logs`;
 CREATE TABLE `video_player_interaction_logs` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `video_id` bigint(20) unsigned DEFAULT NULL,
@@ -99,7 +106,7 @@ CREATE TABLE `video_player_interaction_logs` (
   `temp_id` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 
